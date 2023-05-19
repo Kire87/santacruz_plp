4327c1159abe281130dee5ce1d6bfce3735dc684
Kire87 <erikff7@gmail.com>
2023-05-19 15:24:26 -0300

    First commit. Most players were inserted at the prolog code. Created empty java class Player.

---
+++ prolog/santacruz.pl
@@ -0,0 +1,47 @@
+player(michael).
+player(leo_fernandes).
+player(italo_melo).
+player(guedes).
+player(pingo).
+player(marcus_vinicius).
+player(lucas_silva).
+player(fabricio).
+player(pipico).
+player(chiquinho).
+player(nadson).
+player(geaze).
+player(jadson).
+player(yan_oliveira).
+player(anderson_paulista).
+player(mateus_muller).
+player(italo_henrique).
+player(emerson_galego).
+player(ben_10).
+player(marcelinho).
+player(italo_silva).
+player(maranhao).
+player(dayvid).
+
+number(1, michael).
+number(2, leo_fernandes).
+number(3, italo_melo).
+number(4, guedes).
+number(5, pingo).
+number(6, marcus_vinicius).
+number(7, lucas_silva).
+number(8, fabricio).
+number(9, pipico).
+number(10, chiquinho).
+number(11, nadson).
+number(12, geaze).
+number(13, jadson).
+number(14, yan_oliveira).
+number(15, anderson_paulista).
+number(16, mateus_muller).
+number(17, italo_henrique).
+number(18, emerson_galego).
+number(19, ben_10).
+number(20, marcelinho).
+number(21, italo_silva).
+number(22, maranhao).
+number(23, dayvid).
