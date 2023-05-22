3f400155a5e8b9097f2f9316dc5272a9655b3012
Lucas Branco Alves de Melo <lucasbranco027@protonmail.com>
2023-05-22 15:50:06 -0300

    now java code is working as intended

--- java/src/Main.java
+++ java/src/Main.java
@@ -3,18 +3,18 @@ public class Main {
 		Player[] player = new Player[23];
 
 		player[0] = new Player("Michael", 1, "Goleiro");
-		player[1] = new Player("Léo Fernandes", 2, "Lateral Direito", {"Ponta Direita"});
+		player[1] = new Player("Léo Fernandes", 2, "Lateral Direito", new String[]{"Ponta Direita"});
 		player[2] = new Player("Ítalo Melo", 3, "Zagueiro");
 		player[3] = new Player("Guedes", 4, "Zagueiro");
 		player[4] = new Player("Pingo", 5, "Volante");
 		player[5] = new Player("Marcus Vinícius", 6, "Lateral Esquerdo");
-		player[6] = new Player("Lucas Silva", 7, "Ponta Esquerda", ["Centroavante", "Segundo Atacante"]);
-		player[7] = new Player("Fabrício", 8, "Volante", ["Meia Central", "Lateral Direito"]);
-		player[8] = new Player("Pipico", 9, "Centroavante", ["Meia Ofensivo"]);
-		player[9] = new Player("Chiquinho", 10, "Ponta Esquerda", ["Lateral Esquerdo", "Ponta Direita"]);
-		player[10] = new Player("Nadson", 11, "Meia Ofensivo", ["Segundo Atacante"]);
+		player[6] = new Player("Lucas Silva", 7, "Ponta Esquerda", new String[]{"Centroavante", "Segundo Atacante"});
+		player[7] = new Player("Fabrício", 8, "Volante", new String[]{"Meia Central", "Lateral Direito"});
+		player[8] = new Player("Pipico", 9, "Centroavante", new String[]{"Meia Ofensivo"});
+		player[9] = new Player("Chiquinho", 10, "Ponta Esquerda", new String[]{"Lateral Esquerdo", "Ponta Direita"});
+		player[10] = new Player("Nadson", 11, "Meia Ofensivo", new String[]{"Segundo Atacante"});
 		player[11] = new Player("Geaze", 12, "Goleiro");
-		player[12] = new Player("Jadson", 13, "Lateral Direito", ["Volante"]);
+		player[12] = new Player("Jadson", 13, "Lateral Direito", new String[]{"Volante"});
 		player[13] = new Player("Yan Oliveira", 14, "Zagueiro");
 		player[14] = new Player("Anderson Paulista", 15, "Volante");
 		player[15] = new Player("Mateus Müller", 16, "Lateral Esquerdo");
@@ -22,9 +22,12 @@ public class Main {
 		player[17] = new Player("Emerson Galego", 18, "Atacante");
 		player[18] = new Player("Gabriel Cardoso (Ben 10)", 19, "Centroavante");
 		player[19] = new Player("Marcelinho", 20, "Centroavante");
-		player[20] = new Player("Italo Silva", 21, "Lateral Esquerdo", ["Ponta Esquerda"]);
-		player[21] = new Player("Maranhão", 22, "Ponta Direita", ["Ponta Esquerda", "Meia Ofensivo"]);
-		player[22] = new Player("Dayvid", 23, "Meia Ofensivo", ["Ponta Direita", "Ponta Esquerda"]);
+		player[20] = new Player("Italo Silva", 21, "Lateral Esquerdo", new String[]{"Ponta Esquerda"});
+		player[21] = new Player("Maranhão", 22, "Ponta Direita", new String[]{"Ponta Esquerda", "Meia Ofensivo"});
+		player[22] = new Player("Dayvid", 23, "Meia Ofensivo", new String[]{"Ponta Direita", "Ponta Esquerda"});
 
+		for (Player pl : player) {
+			System.out.println(pl.getName());
+		}
 	}
 }
